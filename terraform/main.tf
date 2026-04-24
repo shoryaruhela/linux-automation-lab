813b1d7f51d4d50fdf4ead6174fc4f78e8320487
Shorya Ruhela <ruhela.shorya@gmail.com>
2026-04-24 13:03:36 +0000

    Initial commit: Terraform + Ansible AWS automation lab

---
+++ terraform/main.tf
@@ -0,0 +1,67 @@
+provider "aws" {
+  region = "eu-west-1"
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  owners      = ["099720109477"]
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
+resource "aws_security_group" "web_sg" {
+  name        = "shorya-web-sg"
+  description = "Allow SSH and HTTP"
+
+  ingress {
+    description = "SSH access"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTP access"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    description = "Allow all outbound"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "linux_vm" {
+  ami                         = data.aws_ami.ubuntu.id
+  instance_type               = "t3.micro"
+  key_name                    = "shorya-key"
+  vpc_security_group_ids      = [aws_security_group.web_sg.id]
+  associate_public_ip_address = true
+
+  tags = {
+    Name = "Shorya-Terraform-Ansible-VM"
+  }
+}
+
+output "public_ip" {
+  value = aws_instance.linux_vm.public_ip
+}
+
+output "ssh_command" {
+  value = "ssh -i ~/shorya-key.pem ubuntu@${aws_instance.linux_vm.public_ip}"
+}
