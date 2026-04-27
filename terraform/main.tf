87056c23a17891aaa5c7c338985b4012710b0c4b
Shorya Ruhela <ruhela.shorya@gmail.com>
2026-04-27 09:00:24 +0000

    Added proper CI/CD pipeline

--- terraform/main.tf
+++ terraform/main.tf
@@ -58,14 +58,14 @@ resource "aws_security_group" "web_sg" {
 
 resource "aws_instance" "linux_vm" {
   ami                         = data.aws_ami.ubuntu.id
-  instance_type               = "t3.micro"
+  instance_type = var.instance_type
   key_name                    = "shorya-key"
   vpc_security_group_ids      = [aws_security_group.web_sg.id]
   associate_public_ip_address = true
 
   tags = {
-    Name = "Shorya-Terraform-Ansible-VM"
-  }
+  Name = "shorya-${var.environment}-vm"
+}
 }
 
 output "public_ip" {
---
+++ terraform/variables.tf
@@ -0,0 +1,7 @@
+variable "instance_type" {
+  default = "t3.micro"
+}
+
+variable "environment" {
+  default = "dev"
+}
